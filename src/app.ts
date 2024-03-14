10bf77e429de3c19af969fec6e7234b172f9b7f2
wayneleon1 <rurangwaleon10@gmail.com>
2024-03-14 16:10:57 +0200

    complete setting up folder structuring

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,30 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
 import "dotenv/config";
-require("dotenv").config();
+
 const app = express();
+require("dotenv").config();
 
-// Middlewares
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-// Routes
+// routes
 app.get("/", (req: Request, res: Response) => {
   res.send("welcome to my backend");
 });
 
 // Connect to DB
-app.listen(process.env.PORT, () => {
-  console.log("You are listening on port http://localhost:3000");
-});
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.USER_NAME}:${process.env.KEY}@backend.a1mvapj.mongodb.net/${process.env.COLLECTION_NAME}?retryWrites=true&w=majority&appName=backend`
+  )
+  .then(() => {
+    console.log("Connected to the Database");
+    app.listen(process.env.PORT, () => {
+      console.log("You are listening on port http://localhost:3000");
+    });
+  })
+  .catch(() => {
+    console.log("Connection fail");
+  });
