99df939734ffc57672692acb85e0a5feb907198b
wayneleon1 <rurangwaleon10@gmail.com>
2024-03-14 15:32:34 +0200

    complete setting up envirnoment

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-console.log("welcome to mybackend");
+import express, { Request, Response } from "express";
+import "dotenv/config";
+require("dotenv").config();
+const app = express();
+
+// Middlewares
+
+// Routes
+app.get("/", (req: Request, res: Response) => {
+  res.send("welcome to my backend");
+});
+
+// Connect to DB
+app.listen(process.env.PORT, () => {
+  console.log("You are listening on port http://localhost:3000");
+});
