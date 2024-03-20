40b99ac085a309e0b31dcbdda033dbf0a57986f3
wayneleon1 <rurangwaleon10@gmail.com>
2024-03-20 08:08:18 -0700

    working on Leave comments on single blog

--- src/controllers/blogComment.controllers.ts
+++ src/controllers/blogComment.controllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
+import mongoose from "mongoose";
 import Blog, { IBlog } from "../models/blog.model";
-import BlogComment, { IBlogComment} from "../models/blogComment.model";
+import BlogComment, { IBlogComment } from "../models/blogComment.model";
 
 export const createComment = async (
   req: Request,
@@ -8,8 +9,18 @@ export const createComment = async (
 ): Promise<void> => {
   let blog_id = req.params.blog_id;
 
+  // checking Blog ID
+  if (!mongoose.Types.ObjectId.isValid(blog_id)) {
+    res.status(400).json({ message: "Invalid Blog ID" });
+    return;
+  }
 
+  // Get blog by Id
+  const blog: IBlog | null = await Blog.findById(blog_id);
+  if (!blog) {
+    res.status(404).json({ message: "Blog not found" });
+    return;
+  }
 
-    
   res.status(200).send({ message: "Comment added successfuly" });
 };
