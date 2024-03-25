11807b22289a8ddf27ac171d199d267b421ee9a9
wayneleon1 <rurangwaleon10@gmail.com>
2024-03-25 09:34:23 +0200

    config global fuction for all tests

---
+++ src/__test__/testUtils.test.ts
@@ -0,0 +1,48 @@
+// testSetup.js
+import request from "supertest";
+import app from "../app";
+import { testConnectToDatabase } from "../config/dbConnection";
+import { loginData, userData } from "../data/static";
+
+import User from "../models/users.model";
+import Blog from "../models/blog.model";
+import Queries from "../models/queries.model";
+import Project from "../models/project.model";
+import Skills from "../models/skills.model";
+import BlogComment from "../models/blogComment.model";
+
+// Function to register and login user
+export async function registerAndLoginUser() {
+  // Register user
+  const { body: registerResponse } = await request(app)
+    .post("/mybrand/user")
+    .send(userData)
+    .expect(201);
+  expect(registerResponse.message).toStrictEqual(
+    "User registered successfully"
+  );
+
+  // Login user
+  const { body: loginResponse } = await request(app)
+    .post("/mybrand/user/login")
+    .send(loginData)
+    .expect(200);
+  expect(loginResponse.accessToken).toBeDefined();
+
+  return loginResponse.accessToken; // Return the token for further use
+}
+
+// Hook to run before all tests
+export async function beforeAllHook() {
+  await testConnectToDatabase();
+}
+
+// Hook to run after all tests
+export async function afterAllHook() {
+  await User.deleteMany();
+  await Queries.deleteMany();
+  await Blog.deleteMany();
+  await BlogComment.deleteMany();
+  await Project.deleteMany();
+  await Skills.deleteMany();
+}
