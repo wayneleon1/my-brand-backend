8fcbb1aafb283bc8cbb67f2f1cedd731192bf00e
wayneleon1 <rurangwaleon10@gmail.com>
2024-03-25 11:45:45 +0200

    restructuring app ts for routes

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import "dotenv/config";
-import skillsRouter from "./routes/skills.routers";
-import projectRouter from "./routes/project.routers";
-import blogRouter from "./routes/blog.routers";
-import userRouter from "./routes/users.routers";
-
-import BlogCommentRouter from "./routes/blogComment.routers";
-import queriesRouter from "./routes/queries.routers";
+import router from "./routes";
 
 const app: Express = express();
 require("dotenv").config();
@@ -15,18 +9,14 @@ require("dotenv").config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// routes
+//Index route
 app.get("/", (req: Request, res: Response) => {
   return res
     .status(200)
     .send({ message: "Welcome to RURANGWA Leo's brand API" });
 });
 
-app.use("/mybrand/skills", skillsRouter);
-app.use("/mybrand/project", projectRouter);
-app.use("/mybrand/blog", blogRouter);
-app.use("/mybrand/user", userRouter);
-app.use("/mybrand/queries", queriesRouter);
-app.use("/mybrand/blogComment", BlogCommentRouter);
+// mybrand Apis route
+app.use("/mybrand", router);
 
 export default app;
